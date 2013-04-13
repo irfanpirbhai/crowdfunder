14dc5a805aff92f097c561b9af8363f0f8acd173
Irfan Pirbhai <irfan.pirbhai@gmail.com>
2013-04-13 11:59:52 -0400

    Add three FactoryGirl instances in projects_flow_test, add links to projects in Project#index

--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -3,10 +3,20 @@ require 'test_helper'
 class ProjectFlowsTest < ActionDispatch::IntegrationTest
   
   test 'Projects list on Project#index page' do
-    FactoryGirl.create(:project)
-    
-    visit(projects_path)
-    assert page.has_content?('The new shoe project')
+    FactoryGirl.create(:project, :title => "Project 1")
+    FactoryGirl.create(:project, :title => "Project 2")
+    FactoryGirl.create(:project, :title => "Project 3")
+
+    visit('/projects')
+    assert projects_path, current_path
+
+    # expect the header
+    assert page.has_content?("Project Listings")
+
+    # expect the projects to list
+    assert page.has_content?('Project 1')
+    assert page.has_content?('Project 2')
+    assert page.has_content?('Project 3')
   end
 
 end
